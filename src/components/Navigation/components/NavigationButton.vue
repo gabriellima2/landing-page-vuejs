d0040b59bad69b35de013c621b769d3739bc7194
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-06-28 19:05:34 -0300

    feat: add navigation-button component

---
+++ src/components/Navigation/components/NavigationButton.vue
@@ -0,0 +1,28 @@
+<template>
+	<button
+		class="navigation-button"
+		:title="props.isOpen ? 'Fechar Menu' : 'Abrir Menu'"
+		@click="handleClick"
+	>
+		{{ props.isOpen ? "Fechar" : "Menu" }}
+	</button>
+</template>
+
+<script setup lang="ts">
+	type NavigationButtonProps = { isOpen: boolean; handleClick: () => void };
+	const props = defineProps<NavigationButtonProps>();
+</script>
+
+<style scoped lang="scss">
+	@import "../../../styles/scss/main";
+	.navigation-button {
+		padding: 8px;
+		color: $font-secondary-color;
+		font-size: $default-font-size;
+		background: none;
+		&:hover,
+		&:focus {
+			color: $font-primary-color;
+		}
+	}
+</style>
