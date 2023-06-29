1a2ac0941cb1b0a6f13bb0f2292a857130465def
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-06-29 17:16:30 -0300

    feat: add platform-data components

---
+++ src/components/PlatformDataDescription.vue
@@ -0,0 +1,16 @@
+<template>
+	<h3 class="platform-data-description"><slot></slot></h3>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+	@import "../styles/scss/main";
+	.platform-data-description {
+		font-family: $secondary-font;
+		font-size: $sm-font-size;
+		text-transform: uppercase;
+		font-weight: 400;
+		color: $font-secondary-color;
+	}
+</style>
---
+++ src/components/PlatformDataValue.vue
@@ -0,0 +1,17 @@
+<template>
+	<h2 :class="`platform-data-value ${props.class}`"><slot></slot></h2>
+</template>
+
+<script setup lang="ts">
+	type PlatformDataValueProps = { class?: string };
+	const props = defineProps<PlatformDataValueProps>();
+</script>
+
+<style scoped lang="scss">
+	@import "../styles/scss/main";
+	.platform-data-value {
+		font-size: $xxl-font-size;
+		text-transform: uppercase;
+		font-weight: 400;
+	}
+</style>
