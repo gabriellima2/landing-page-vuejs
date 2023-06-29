6944a9d9c43e79fc23324945c727455b7a7a491b
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-06-29 18:42:20 -0300

    style: renames platform components to community

--- src/components/CommunityDataDescription.vue
+++ src/components/CommunityDataDescription.vue
@@ -1,12 +1,12 @@
 <template>
-	<h3 class="platform-data-description"><slot></slot></h3>
+	<h3 class="community-data-description"><slot></slot></h3>
 </template>
 
 <script setup lang="ts"></script>
 
 <style scoped lang="scss">
 	@import "../styles/scss/main";
-	.platform-data-description {
+	.community-data-description {
 		font-family: $secondary-font;
 		font-size: $sm-font-size;
 		text-transform: uppercase;
--- src/components/CommunityDataValue.vue
+++ src/components/CommunityDataValue.vue
@@ -1,15 +1,15 @@
 <template>
-	<h2 :class="`platform-data-value ${props.class}`"><slot></slot></h2>
+	<h2 :class="`community-data-value ${props.class}`"><slot></slot></h2>
 </template>
 
 <script setup lang="ts">
-	type PlatformDataValueProps = { class?: string };
-	const props = defineProps<PlatformDataValueProps>();
+	type CommunityDataValueProps = { class?: string };
+	const props = defineProps<CommunityDataValueProps>();
 </script>
 
 <style scoped lang="scss">
 	@import "../styles/scss/main";
-	.platform-data-value {
+	.community-data-value {
 		font-size: $xxl-font-size;
 		text-transform: uppercase;
 		font-weight: 400;
