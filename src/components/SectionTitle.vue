67b5fb75387bc166f5662bbc4a0d2c36cf50f79c
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-06-29 17:15:58 -0300

    feat: add section-title component

---
+++ src/components/SectionTitle.vue
@@ -0,0 +1,18 @@
+<template>
+	<h1 class="section-title" v-bind="props"><slot></slot></h1>
+</template>
+
+<script setup lang="ts">
+	import { HTMLAttributes } from "vue";
+
+	interface SectionTitleProps extends /* @vue-ignore */ HTMLAttributes {}
+	const props = defineProps<SectionTitleProps>();
+</script>
+
+<style scoped lang="scss">
+	@import "../styles/scss/main";
+	.section-title {
+		font-size: $xl-font-size;
+		font-weight: 400;
+	}
+</style>
