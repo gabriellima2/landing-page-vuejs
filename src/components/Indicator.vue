46b8065ba20bce009138cc183e753d7013620245
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-07-01 15:19:12 -0300

    feat: add indicator component

---
+++ src/components/Indicator.vue
@@ -0,0 +1,14 @@
+<template>
+	<span class="indicator"></span>
+</template>
+
+<style scoped lang="scss">
+	@import "../styles/scss/main";
+	.indicator {
+		display: block;
+		width: 4px;
+		height: 4px;
+		border-radius: 100%;
+		background-color: $util-light-blue-color;
+	}
+</style>
