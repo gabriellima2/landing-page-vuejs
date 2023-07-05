664e8ad5d6ca22c1820e03a41ed15f24ed8df461
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-07-05 14:44:41 -0300

    feat: add copyright component

---
+++ src/components/Copyright.vue
@@ -0,0 +1,11 @@
+<template>
+	<small class="copyright">© 2023 Solana Foundation. All rights reserved</small>
+</template>
+
+<style scoped lang="scss">
+	@import "../styles/scss/main";
+	.copyright {
+		color: $font-secondary-color;
+		font-size: $sm-font-size;
+	}
+</style>
