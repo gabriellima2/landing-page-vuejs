d3132f49e3b2e8e0e8e0c4d5be691bb50ce047ed
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-07-05 15:35:20 -0300

    feat: add language component

---
+++ src/components/Language.vue
@@ -0,0 +1,20 @@
+<template>
+	<button class="language">
+		<img src="/language-icon.svg" alt="Language icon" />
+		EN
+	</button>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+	@import "../styles/scss/main";
+	.language {
+		display: flex;
+		gap: 8px;
+		color: $font-secondary-color;
+		font-size: $sm-font-size;
+		text-transform: uppercase;
+		background: none;
+	}
+</style>
