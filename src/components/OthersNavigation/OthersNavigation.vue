12c6dfee9387de1e96093e0d25d5fb26076f5ec3
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-07-05 15:12:24 -0300

    feat: add others-navigation component

---
+++ src/components/OthersNavigation/OthersNavigation.vue
@@ -0,0 +1,44 @@
+<template>
+	<section class="others-navigation">
+		<h1 class="others-navigation__title">
+			{{ title }}
+		</h1>
+		<ul class="others-navigation__list">
+			<OthersNavigationItem
+				v-for="item of items"
+				:key="item.text"
+				v-bind="item"
+			/>
+		</ul>
+	</section>
+</template>
+
+<script setup lang="ts">
+	import OthersNavigationItem, {
+		type OthersNavigationItemProps,
+	} from "./components/OthersNavigationItem.vue";
+	type OthersNavigationProps = {
+		title: string;
+		items: OthersNavigationItemProps[];
+	};
+	defineProps<OthersNavigationProps>();
+</script>
+
+<style scoped lang="scss">
+	@import "../../styles/scss/main";
+	.others-navigation {
+		display: flex;
+		flex-direction: column;
+		gap: 12px;
+	}
+	.others-navigation__title {
+		font-weight: 400;
+		font-size: $sm-font-size;
+		text-transform: uppercase;
+	}
+	.others-navigation__list {
+		display: flex;
+		flex-direction: column;
+		gap: 8px;
+	}
+</style>
---
+++ src/components/OthersNavigation/components/OthersNavigationItem.vue
@@ -0,0 +1,24 @@
+<template>
+	<li>
+		<a class="others-navigation-item" :href="text" :title="text">{{ text }}</a>
+	</li>
+</template>
+
+<script setup lang="ts">
+	import type { Link } from "../../../@types/Link";
+	export type OthersNavigationItemProps = Omit<Link, "text"> & { text: string };
+	defineProps<OthersNavigationItemProps>();
+</script>
+
+<style scoped lang="scss">
+	@import "../../../styles/scss/main";
+	.others-navigation-item {
+		text-transform: capitalize;
+		color: $font-secondary-color;
+		font-size: $sm-font-size;
+		&:hover,
+		&:focus {
+			text-decoration: underline;
+		}
+	}
+</style>
